0a75fb55db576434ddac876a8179d3a5a98138b5
mrHoft <ru.hoft@gmail.com>
2025-02-18 23:15:12 +0300

    fix: request.url

--- src/router/plain.ts
+++ src/router/plain.ts
@@ -5,9 +5,8 @@ import { requestLog } from "~/src/utils/log.ts";
 export function plain({ request, response }: Context) {
   requestLog(`Plain badge`, request);
 
-  const url = new URL(request.url);
-  const title = url.searchParams.get("title") ?? undefined;
-  const color = url.searchParams.get("color") ?? undefined;
+  const title = request.url.searchParams.get("title") ?? undefined;
+  const color = request.url.searchParams.get("color") ?? undefined;
 
   response.type = "image/svg+xml; charset=utf-8";
   response.body = badgePlain(title, color);
