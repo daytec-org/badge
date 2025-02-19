9705b6a4119a7a4db1677b4bd6a6c0c3d764edec
mrHoft <ru.hoft@gmail.com>
2025-02-20 00:29:42 +0300

    feat: badge skill

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'jsr:@oak/oak/router'
-import { plain } from '~/src/router/plain.ts'
 import { favicon } from '~/src/router/favicon.ts'
+import { plain } from '~/src/router/plain.ts'
+import { skill } from '~/src/router/skill.ts'
 
 const router = new Router()
-router.get('/plain', plain)
 router.get('/favicon.ico', favicon)
+router.get('/plain', plain)
+router.get('/skill', skill)
 
 export default router
