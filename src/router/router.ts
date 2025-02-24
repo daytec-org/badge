e989638657da95a847c912765a916d77cac9618f
mrHoft <ru.hoft@gmail.com>
2025-02-24 13:25:37 +0300

    feat: main page

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'jsr:@oak/oak/router'
+import { handleMainPage } from '~/src/router/page.ts'
 import { handleFavicon } from '~/src/router/favicon.ts'
 import { handleBadgePlain } from '~/src/router/plain.ts'
 import { handleBadgeSkill } from '~/src/router/skill.ts'
 import { handleStack } from '~/src/router/stack.ts'
 
 const router = new Router()
+router.get('/', handleMainPage)
 router.get('/favicon.ico', handleFavicon)
 router.get('/plain', handleBadgePlain)
 router.get('/skill', handleBadgeSkill)
