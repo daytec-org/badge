79e1280d888ad293ca8d07ec12b4f10d6c94dba0
Евгения Репникова <evrepni@gmail.com>
2025-07-17 01:52:59 +0500

    feat: theme switcher

--- src/client/ui/header/header.tsx
+++ src/client/ui/header/header.tsx
@@ -3,20 +3,26 @@ import React from 'https://esm.sh/react@18.2.0'
 const API_URL = 'http://localhost:3000' //'https://badge-service.deno.dev'
 
 export const Header = () => {
+  const [theme, setTheme] = React.useState('dark')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = event.target
+    const newTheme = checked ? 'dark' : 'light'
+    setTheme(newTheme)
+    document.documentElement.className = `theme-${newTheme}`
+  }
+
   return (
     <header className="header">
-      {/* <div className="preview">
-        <img src={`${API_URL}/favicon.ico`} alt="logo" />
-      </div>
-      
-      */}
-
       <a href="/" className="header__title">
-        <img className="header__logo" src={`${API_URL}/favicon.ico`} alt="logo" />
+        <div className="header__logo">
+          <img src={`${API_URL}/favicon.ico`} alt="logo" />
+        </div>
         <div> Badge service</div>
       </a>
       <div className="header__theme">
-        <img src={`${API_URL}/img/moon`} alt="logo" />
+        <input className="header__theme_switcher" type="checkbox" checked={theme === 'dark'} onChange={handleChange} />
+        <img src={`${API_URL}/img/${theme}`} alt="theme" />
       </div>
     </header>
   )
