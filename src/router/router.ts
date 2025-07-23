eee40b69e6020741b765b42e82d08c71a8d42caf
mrHoft <ru.hoft@gmail.com>
2025-07-23 06:56:35 +0300

    feat: route icons

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,12 +5,15 @@ import { handleSource } from '~/src/router/client/src.ts'
 import { handleBadgePlain } from '~/src/router/badge/plain.ts'
 import { handleBadgeSkill } from '~/src/router/badge/skill.ts'
 import { handleStack } from '~/src/router/badge/stack.ts'
+import { handleIconList } from '~/src/router/service/icons.ts'
 
 const router = new Router()
 // Badges
 router.get('/plain', handleBadgePlain)
 router.get('/skill', handleBadgeSkill)
 router.get('/stack', handleStack)
+// Services
+router.get('/icons', handleIconList)
 // Main page
 router.get('/', handleMainPage)
 router.get('/favicon.ico', handleFavicon)
