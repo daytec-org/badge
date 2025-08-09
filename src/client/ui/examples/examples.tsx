6791d60835d5bb7e9a8ac5c7b630fa9c2df6e50d
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-08-09 23:05:03 +0500

    refactor: move examples to component

---
+++ src/client/ui/examples/examples.tsx
@@ -0,0 +1,39 @@
+import React from 'https://esm.sh/react@18.2.0'
+import { ENV } from '@/config'
+
+const { API_URL } = ENV
+
+export const Examples = () => {
+  return (
+    <div className="home__examples">
+      <h3 className="home__title">Examples</h3>
+      <ul className="list__badges">
+        <li className="home__badge_item">
+          <h3>Plain badges</h3>
+          <div className="preview">
+            <img src={`${API_URL}/plain/?title=Deno&color=green&icon=deno&value=2.1`} />
+          </div>
+          <code className="home__code"> /plain/?title=Deno&color=green&icon=deno&value=2.1</code>
+        </li>
+        <li className="home__badge_item">
+          <h3>Skill badges</h3>
+          <div className="preview">
+            <img src={`${API_URL}/skill/?title=React.js&icon=react&value=50`} />
+          </div>
+          <code className="home__code">/skill/?title=React.js&icon=react&value=50</code>
+        </li>
+        <li className="home__badge_item">
+          <h3>Stack badges</h3>
+          <div className="preview">
+            <img
+              src={`${API_URL}/stack/?title=HTML5&value=75&icon=html;title=CSS&value=35&icon=css;title=React.js&icon=react&value=50`}
+            />
+          </div>
+          <code className="home__code">
+            /stack/?title=HTML5&value=75&icon=html;title=CSS&value=35&icon=css;title=React.js&icon=react&value=50
+          </code>
+        </li>
+      </ul>
+    </div>
+  )
+}
